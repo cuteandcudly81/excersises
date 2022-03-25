b2928e3257978a2a7314c8c4c6be40b4f8e8e8af
cuteandcudly81 <muhammed_yusuf@hotmail.com>
2022-03-25 10:51:16 +0100

    Initial commit

---
+++ src/main/java/se/lexikon/Leapyear.java
@@ -0,0 +1,15 @@
+package se.lexikon;
+
+public class Leapyear
+{
+    public static void main(String[]args) {
+        int year = 2022;
+
+        if((year%400 ==0) ||(year%4 == 0) && (year % 100 !=0)){
+            System.out.println(year + " is a leap year");
+            }
+            else{
+            System.out.println(year + " is not a leap year");
+            }
+    }
+}
