5e2208340c2067eca8311d5739ce15b16ad3b346
cuteandcudly81 <muhammed_yusuf@hotmail.com>
2022-03-25 15:45:30 +0100

    math

---
+++ src/main/java/se/lexikon/Averageofthree.java
@@ -0,0 +1,21 @@
+package se.lexikon;
+
+import java.util.Scanner;
+
+public class Averageofthree {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in) ;
+        System.out.println("Enter number1: ");
+        int number1 = sc.nextInt();
+        System.out.println("Enter number2: ");
+        int number2 = sc.nextInt();
+        System.out.println("Enter number3: ");
+        int number3 = sc.nextInt();
+        System.out.println("Enter number4: ");
+        int number4 = sc.nextInt();
+
+
+
+    }
+}
---
+++ src/main/java/se/lexikon/Math.java
@@ -0,0 +1,27 @@
+package se.lexikon;
+
+public class Math
+{
+    public static void main(String[]args) {
+        /*
+        int result = 45 + 11;
+        System.out.println(result);
+        */
+        /*
+        int number1 = 55;
+        int number2 = 12;
+        int result = number1 - number2;
+        System.out.println(result);
+        */
+        /*
+        int number1 = 12;
+        int result = number1 * 4;
+        System.out.println(result);
+        */
+        /*
+        double result = 24 / 6;
+        System.out.println(result);
+        */
+
+    }
+}
