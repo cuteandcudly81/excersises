d0cc18bbe85c103f83566cd1bc8dd769edb59d31
cuteandcudly81 <muhammed_yusuf@hotmail.com>
2022-03-27 15:39:55 +0200

    Averageof

--- src/main/java/se/lexikon/Average.java
+++
@@ -1,4 +0,0 @@
-package se.lexikon;
-
-public class Average {
-}
---
+++ src/main/java/se/lexikon/Averageof.java
@@ -0,0 +1,17 @@
+package se.lexikon;
+
+import java.util.Scanner;
+
+public class Averageof {
+
+    public static void main(String[]args) {
+        int number1 = 23;
+        int number2 = 11;
+        int number3 = 77;
+        System.out.println((number1 + number2 + number3) / 3);
+
+    }
+}
+
+
+
