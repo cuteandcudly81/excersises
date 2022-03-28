1e641d5cc8bfc49ff4b91ee94bd0a39d8fc081d1
cuteandcudly81 <muhammed_yusuf@hotmail.com>
2022-03-28 08:38:32 +0200

    seconds

--- src/main/java/se/lexikon/GuessAgain.java
+++ src/main/java/se/lexikon/GuessAgain.java
@@ -1,5 +1,31 @@
 package se.lexikon;
 
-public class GuessAgain
-{
+import java.util.Random;
+import java.util.Scanner;
+
+public class GuessAgain {
+
+    public static void main (String[]args) {
+        Scanner scan = new Scanner (System.in);
+        Random rand = new Random();
+        int number = rand.nextInt(  500);
+        int guesses = 0;
+        while (true) {
+            guesses++;
+            int guess = scan.nextInt();
+
+            if(guess == number) {
+                break;
+            }
+            if(guess > number) {
+                System.out.println("Your guess is too big");
+            } else {
+                System.out.println("Your guess is too small");
+            }
+        }
+
+        System.out.println("You won with " + guesses + " guesses. ");
+
+
+    }
 }
